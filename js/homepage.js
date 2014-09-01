1a297a7efdde11ff4b1bba64d1ec855fc7f31b8f
Mohdhar Noor <mohdhar.noor@gmail.com>
2014-09-01 15:43:19 -0400

    Got a side bar going, soon to add content

--- js/homepage.js
+++ js/homepage.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
 
-	$("#navlist").on("click", "li", function(){
-		//find navLinks with a class of navlist_select
-		$("#navlist").find("li.navlist_select")
-			.removeClass("navlist_select");
-		$(this).addClass("navlist_select");
+	//calculate side bar height
+	var sideBarHeight = $(window).height() - $("#header").outerHeight();
+	$("#sidebar").css("min-height", sideBarHeight);
+
+	//initially highlight the home nav option when page loads
+	$("#topnav").find("li").first().addClass("topnav_select");
+
+	$("#topnav").on("click", "li", function(){
+		//find navLinks with a class of topnav_select
+		var highlighted = $("#topnav").find("li.topnav_select");
+		$("#sidebar").find("#" + $(highlighted).data("link")).fadeOut();
+		$(highlighted).removeClass("topnav_select");
+
+		$(this).addClass("topnav_select");
+		$("#sidebar").find("#" + $(this).data("link")).fadeIn();
 	});
+
 });
