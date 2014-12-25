f311581601c41bdae1136ed6ca9e858629b9c788
Mohdhar Noor <mohdhar.noor@gmail.com>
2014-12-24 21:28:31 -0500

    Finished the details of the navigation menu, added a title and logo

--- js/landing.js
+++ js/landing.js
@@ -1,5 +1,12 @@
-$(document).ready(function(){
-	
+$(document).ready(function() {
+    //animate hand indicating position of nav menu
+    handAnimation = setInterval(function(){$('.menu_item:first-child').toggleClass('move_hand');}, 250);
+
+    //
+    $('#nav_menu').on('mousenter', function(){
+        clearInterval(handAnimation);
+    });
+
 	//display popout panel
 	$('.menu_item').on('mouseenter', function(){
 		$(this).find('.popout_panel').fadeIn(300);
